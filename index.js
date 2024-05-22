6871f87464ae148ad28995694fabf3a454632bdb
Juanchi <toranzos628@gmail.com>
2024-05-22 15:44:30 -0300

    cambios listos back funcionando

--- index.js
+++ index.js
@@ -2,15 +2,12 @@ const express = require('express');
 const cors = require('cors');
 const dbConnection = require('./database/config'); // Verifica que la ruta sea correcta
 const app = express();
-
 // Lectura y parseo del body
 app.use(express.json());
 app.use(cors());
 app.use('/admin', require('./router/adminRouter')); // Asegúrate de que la ruta sea correcta
-
 // Conexión a la base de datos
 dbConnection();
-
 // Definimos el puerto y ejecutamos el callback para iniciar el servidor
 app.listen(4000, () => {
     console.log('Servidor corriendo en el puerto 4000');
